2d5ebb0e9b4ae8006e53b174e9cc079737c7ee8e
Kostiuchenko Maksym <maksym.kostiuchenko@masterofcode.com>
2019-10-21 20:14:10 +0300

    first task

---
+++ js-check/1.js
@@ -0,0 +1,10 @@
+const my_number = -1;
+myNumber = 0;
+var number = 3;
+if (true) {
+    let my_number = 1;
+    var myNumber = 2;
+    var number = 6;
+}
+const sum = my_number + myNumber + number; // Result: -1 + 2 + 6 = 7
+console.log(sum);
