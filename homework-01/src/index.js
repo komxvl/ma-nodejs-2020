b2795438f20fe3d11f4f51fd5a03fe3fe427624e
max <maksym.kostiuchenko@masterofcode.com>
2019-11-08 17:20:20 +0200

    added editconfig file

--- homework-01/src/index.js
+++ homework-01/src/index.js
@@ -13,4 +13,4 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () =>
   console.log(`Server is running at http://${hostname}:${port}/`),
 );
-console.log("Test")
+console.log('Test');
