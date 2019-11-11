bed3e196233d6481164baeedcd9b4cf4657baa8c
max <maksym.kostiuchenko@masterofcode.com>
2019-11-11 11:15:15 +0200

    fixes task#2 & task#6

--- js-check/2.js
+++ js-check/2.js
@@ -20,40 +20,43 @@ const user = {
 };
 
 
-for (var objKey in user) {
-    if (objKey == 'firstName' && typeof user[objKey] == 'string') {
-        console.log('User firstname is ok');
-    }
-    if (objKey == 'lastName' && typeof user[objKey] == 'string') {
-        console.log('User lastName is ok');
-    }
-    if (objKey == 'rate' && !isNaN(user[objKey]) && (user[objKey] >= 0 && user[objKey] <= 1)) {
-        console.log('Rate good');
-    }
-    if (objKey == 'address' && user[objKey]) {
-        console.log('User address is ok');
-        for (let objAddress in user[objKey]) {
-            console.log(objAddress)
-            if (objAddress == 'line1' && typeof user[objKey][objAddress] == 'string') {
-                console.log('User line1 is ok');
-            }
-            if (objAddress == 'line2' && typeof user[objKey][objAddress] == 'string') {
-                console.log('User line2 is ok');
-            }
-            if (objAddress == 'city' && typeof user[objKey][objAddress] == 'string') {
-                console.log('User city is ok');
+if(user.hasOwnProperty('firstName') || user.hasOwnProperty('lastName') || user.hasOwnProperty('rate') || 
+user.hasOwnProperty('address') || user.hasOwnProperty('phoneNumbers') ){
+    for (var objKey in user) {
+        if (objKey == 'firstName' && typeof user[objKey] == 'string') {
+            console.log('User firstname is ok');
+        }
+        if (objKey == 'lastName' && typeof user[objKey] == 'string') {
+            console.log('User lastName is ok');
+        }
+        if (objKey == 'rate' && !isNaN(user[objKey]) && (user[objKey] >= 0 && user[objKey] <= 1) && typeof user[objKey] === 'number') {
+            console.log('Rate good');
+        }
+        if (objKey == 'address' && typeof user[objKey] === 'object') {
+            console.log('User address is ok');
+            for (let objAddress in user[objKey]) {
+                console.log(objAddress)
+                if (objAddress == 'line1' && typeof user[objKey][objAddress] == 'string') {
+                    console.log('User line1 is ok');
+                }
+                if (objAddress == 'line2' && typeof user[objKey][objAddress] == 'string') {
+                    console.log('User line2 is ok');
+                }
+                if (objAddress == 'city' && typeof user[objKey][objAddress] == 'string') {
+                    console.log('User city is ok');
+                }
             }
         }
-    }
-    if (objKey == 'phoneNumbers' && user[objKey].length > 0) {
-        console.log('User phoneNumbers is ok', user[objKey]);
-        user[objKey].forEach((element, i) => {
-            console.log("ELEMENT", element)
-            if (element.type && element.number &&
-                element.type == ('MOBILE' || 'LINE' || 'VIOP') &&
-                /^\([0-9]{3}\)\s[0-9]{3}-[0-9]{4}$/.test(element.number)) {
-                console.log('good')
-            }
-        });
+        if (objKey == 'phoneNumbers' && user[objKey].length > 0) {
+            console.log('User phoneNumbers is ok', user[objKey]);
+            user[objKey].forEach((element, i) => {
+                console.log("ELEMENT", element)
+                if (element.type && element.number &&
+                    element.type.includes('MOBILE') || element.type.includes('LINE') || element.type.includes('VIOP') &&
+                    /^\(\d{3}\)\s\d{3}-\d{4}$/.test(element.number)) {
+                    console.log('good')
+                }
+            });
+        }
     }
 }
--- js-check/6.js
+++ js-check/6.js
@@ -3,9 +3,8 @@ const second = [6, 7, 8, 9, 0];
 
 const array3 = new Array();
 
-for (var i = 0; i < first.length; i++) {
-    array3.push(second[i]);
+for (var i = first.length-1; i >= 0; i--) {
     array3.push(first[i]);
-
+    array3.push(second[i]);
 }
-console.log(array3.reverse());
+console.log(array3);
