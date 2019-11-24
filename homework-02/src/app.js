576f438cd8408a5eb30ee8c39812f7bebfd29882
Kostiuchenko Maksym <maksym.kostiuchenko@masterofcode.com>
2019-11-24 21:20:36 +0200

    fixes

--- homework-02/src/app.js
+++ homework-02/src/app.js
@@ -2,10 +2,10 @@ const { task1: sumModule, task2: earthModule, task3: promiseModule } = require('
 
 const boot = async () => {
   sumModule.sum(1, 2, 6);
-  earthModule.planetEart.getPlaneValue();
   await promiseModule.setPromise.then((event) => {
     console.log(event);
   });
+  earthModule.planetEart.getPlaneValue();
 };
 
 boot();
