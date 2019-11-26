ae7fc4dd209f084b5c68422dc60210edf8a63d08
Kostiuchenko Maksym <maksym.kostiuchenko@masterofcode.com>
2019-11-26 21:28:50 +0200

    added interval callback

--- homework-03/src/app.js
+++ homework-03/src/app.js
@@ -1,12 +1,16 @@
 const isPrime = require('./isPrime');
 
 let counter = 0;
-let latestPrime = 0;
+let latestPrimeNumber = 0;
 
 setInterval(() => {
   counter++;
   if (isPrime(counter)) {
-    latestPrime = counter;
-    console.log(latestPrime);
+    latestPrimeNumber = counter;
+    console.log(latestPrimeNumber);
   }
 }, 0);
+
+setInterval(() => {
+  console.log(`${+new Date()} : -- IN PROCESS -- Biggest prime number found: ${latestPrimeNumber}`);
+}, 1000);
